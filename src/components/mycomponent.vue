38303c6c60e4298b1d74bbc9f6548480db75b1be
Rusal84 <rusal@bk.ru>
2021-01-04 19:47:29 +0300

    init

---
+++ src/components/mycomponent.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h1>{{ text }}</h1>
+    <form>
+      <input v-model="text">
+      <button @click="onSendClick">
+        нажми меня
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data: () => ({
+    text: '',
+  }),
+  methods: {
+    // Реакция на кнопку отправки
+    async onSendClick() {
+      this.text = this.text + "s"
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
